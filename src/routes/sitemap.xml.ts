39854311682af060842ecbb3b71b1469a1eacfc7
yoshikouki <yoshikouki@gmail.com>
2026-06-11 14:24:12 +0900

    feat(sitemap): replace inline handler with createSitemapApp helper

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -1,24 +1,3 @@
-import { Hono } from "hono";
-import type { AppEnv } from "@/framework/types";
+import { createSitemapApp } from "@/framework/handlers/sitemap";
 
-const app = new Hono<AppEnv>();
-
-app.get("/", (c) => {
-  const manifest = c.var.routeManifest;
-  const baseUrl = "https://example.com"; // TODO: derive from SiteConfig
-  const xml = [
-    '<?xml version="1.0" encoding="UTF-8"?>',
-    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-    ...manifest.map(
-      (entry) =>
-        `  <url><loc>${baseUrl}${entry.path}</loc>${entry.date ? `<lastmod>${entry.date}</lastmod>` : ""}</url>`
-    ),
-    "</urlset>",
-  ].join("\n");
-
-  return new Response(xml, {
-    headers: { "Content-Type": "application/xml" },
-  });
-});
-
-export default app;
+export default createSitemapApp();
